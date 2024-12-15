61310b408ef3debb2f8470def1aeaf66574ccde4
audo-wrld <audo-wrld@kali>
2024-12-15 09:14:28 +0300

    Contact Initialization

--- dialogueworks/urls.py
+++ dialogueworks/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage, name='homepage'),
+    path('contact/', views.contact, name='contact'),
 ]
 
--- dialogueworks/views.py
+++ dialogueworks/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'dialogueswork/homepage.html')
+def contact(request):
+    return render(request, 'dialogueswork/contact.html')
