ea00a22a088950d1a55eb505fbe611f1be5e3dbb
audo-wrld <audo-wrld@kali>
2024-12-23 06:15:11 +0300

    Addition about url, function added

--- dialogueworks/views.py
+++ dialogueworks/views.py
@@ -4,3 +4,5 @@ def homepage(request):
     return render(request, 'dialogueswork/homepage.html')
 def contact(request):
     return render(request, 'dialogueswork/contact.html')
+def about(request):
+    return render(request, 'dialogueswork/about.html')
